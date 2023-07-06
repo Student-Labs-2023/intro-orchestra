f21c25485fd83d77339f2a5914d1dd1fd8bbe535
Farkk <blazem.show@mail.ru>
2023-07-06 22:06:26 +0600

    add commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <h1>Hello!</h1>
       </div>
     </main>
   )
