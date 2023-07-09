936bbe263f7af66d6045968197a022a55cc4c325
Farkk <blazem.show@mail.ru>
2023-07-09 22:38:49 +0600

    add-tailwindCss

--- src/components/home/header/TheHeader.tsx
+++ src/components/home/header/TheHeader.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./header.module.css";
+
 
 export const TheHeader = () => {
   return (
-    <header className={styles.header}>
+    <header className="flex justify-between items-center gap-[25px] flex-wrap pr-[50px] pt-[50px] pl-[50px] mb-[200px]">
       <Link href="https://omfil.ru" style={{ zIndex: "2" }}>
         <Image
           src={"/logo.svg"}
