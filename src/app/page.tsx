724ef5eed99ee1f5d16a706cc415f23d8e9826db
Farkk <blazem.show@mail.ru>
2023-07-31 14:56:34 +0600

    fix-home-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,23 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <>
-      <Link 
-      href={'/panorama'} 
-      style={
-        { position: "absolute", display: "block", width: "100%", height: "100%", zIndex: '1' }
-      }/>
+      <Link
+        href={"/panorama"}
+        style={{
+          position: "absolute",
+          display: "block",
+          width: "100%",
+          height: "100%",
+          zIndex: "1",
+        }}
+      />
+      <Image
+        className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+        src={"/main-image.svg"}
+        width={1610}
+        height={1080}
+        alt={"Картинка"}
+      />
       <Home />
     </>
   );
