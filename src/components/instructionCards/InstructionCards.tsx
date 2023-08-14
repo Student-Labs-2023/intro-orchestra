0f230bd0e886c4ef69daf02149a6cca891e3c3c9
Farkk <blazem.show@mail.ru>
2023-08-14 12:04:05 +0600

    addAdaptiveInstructionCards

--- src/components/instructionCards/InstructionCards.tsx
+++ src/components/instructionCards/InstructionCards.tsx
@@ -8,7 +8,7 @@ import uuid from "react-uuid";
 import { cardsData, buttonsData } from "./DataInstruction";
 import styles from "./instructionCards.module.css";
 
-const geometria = localFont({ src: "../../fonts/Geometria.ttf" });
+const geometria = localFont({ src: "../../fonts/Geometria.woff" });
 const geometriaBold = localFont({ src: "../../fonts/Geometria-Bold.woff" });
 const geometriaMedium = localFont({
   src: "../../fonts/Geometria-Medium.woff",
@@ -54,10 +54,7 @@ const InstructionCards = () => {
         return (
           <>
             {beforeSpan}
-            <span
-              style={geometriaMedium.style}
-              className={"italic"}
-            >
+            <span style={geometriaMedium.style} className={"italic"}>
               {spanText}
             </span>
             {afterSpan}
@@ -71,10 +68,10 @@ const InstructionCards = () => {
   return (
     <>
       <header>
-        <div className="absolute z-[2] top-0 left-0 mt-[30px] ml-[30px]">
+        <div className="absolute z-[2] top-0 left-0 mt-[15px] ml-[15px] lg:mt-[25px] lg:ml-[25px] xl:mt-[30px] xl:ml-[30px]">
           <Link href={"/"} className={styles.exit_btn}></Link>
         </div>
-        <div className="absolute z-[2] top-0 right-0 mt-[30px] mr-[30px]">
+        <div className="absolute z-[2] top-0 right-0 mt-[15px] mr-[15px] lg:mt-[25px] lg:ml-[25px] xl:mt-[30px] xl:ml-[30px]">
           <button
             className={styles.instruction_btn}
             onClick={changeVisibleInstruction}
@@ -82,28 +79,30 @@ const InstructionCards = () => {
         </div>
       </header>
 
-      <main>
+      <main className="relative w-screen h-screen">
         {!backToPanorama && (
-          <div className="absolute w-full h-full bg-[#42424299]/[0.6] z-[3]">
-            <div className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] max-w-[800px] p-[16px] rounded-[24px] bg-white border-[1px] border-[#D93284]">
-              <div className="flex justify-between items-center mb-[8px]">
-                <div className="py-[7px]">
-                  <div className="flex gap-[3px]">
-                    {buttonsData.map((button) => (
-                      <div
-                        key={uuid()}
-                        className={`${
-                          button === cardID
-                            ? "w-[14px]  opacity-[1]"
-                            : "w-[6px] opacity-[0.5]"
-                        } h-[6px] rounded-full  bg-[#D93284]`}
-                      ></div>
-                    ))}
-                  </div>
+          <div className=" absolute w-full h-full bg-[#42424299]/[0.6] z-[3]">
+            <div
+              className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[368px] p-[16px] bg-white border-[2px] border-[#D93284] rounded-[16px] md:max-w-[503px] md:p-[24px] md:rounded-[22px] lg:max-w-[574px] lg:py-[24px] lg:px-[32px] lg:rounded-[16px] lg:border-[3px]
+              2xl:max-w-[800px] 2xl:py-[32px] 2xl:px-[40px] 2xl:rounded-[24px]"
+            >
+              <div className="flex items-center justify-between mb-[8px] md:mb-[12px] lg:mb-[16px] 2xl:mb-[24px]">
+                <div className="flex gap-[3px] py-[5px] md:py-[8px] 2xl:py-[10px] 2xl:gap-[5px]">
+                  {buttonsData.map((btn) => (
+                    <div
+                      key={uuid()}
+                      className={`${
+                        btn === cardID
+                          ? "w-[20px] md:w-[24px] 2xl:w-[32px] opacity-100"
+                          : "w-[8px] md:w-[10px] 2xl:w-[14px] opacity-50"
+                      } h-[8px] bg-[#D93284] rounded-[5px] md:h-[10px] md:rounded-[7px] 2xl:h-[14px] 2xl:rounded-[10px]`}
+                    ></div>
+                  ))}
                 </div>
                 <button
                   style={geometria.style}
-                  className="text-[12px] font-normal leading-[15px] p-[6px]"
+                  className="text-[12px] leading-[10px] p-[5px] text-[#424242] md:text-[14px] md:p-[8px] lg:text-[18px] lg:leading-[16.2px]
+                  2xl:text-[24px] 2xl:leading-[24px] 2xl:p-[10px]"
                   onClick={changeVisibleInstruction}
                 >
                   Закрыть
@@ -113,10 +112,14 @@ const InstructionCards = () => {
               {cardsData.map((card) => {
                 if (card.id === cardID) {
                   return (
-                    <div key={uuid()} className="mb-[16px]">
+                    <div
+                      key={uuid()}
+                      className="min-w-[336px] text-[#141414] mb-[16px] md:min-w-[455px] md:mb-[24px] lg:min-w-[510px] lg:mb-[32px] 2xl:min-w-[720px] 2xl:mb-[40px]"
+                    >
                       <h3
                         style={geometriaBold.style}
-                        className="mb-[3px] text-[16px] leading-[20px] whitespace-nowrap"
+                        className="text-[16px] leading-[20px] font-bold mb-[4px] md:text-[20px] md:leading-[25px] md:mb-[6px] lg:text-[22px] lg:leading-[27.5px]
+                        2xl:text-[34px] 2xl:leading-[42.5px] 2xl:mb-[8px]"
                       >
                         {card.title}
                       </h3>
@@ -125,7 +128,8 @@ const InstructionCards = () => {
                           <p
                             key={uuid()}
                             style={geometria.style}
-                            className="text-[15px] leading-[19px] whitespace-nowrap"
+                            className="text-[14px] leading-[17.5px] font-normal md:text-[18px] md:leading-[22.5px] lg:text-[20px] lg:leading-[25px]
+                            2xl:text-[30px] 2xl:leading-[37.5px]"
                           >
                             {findAndWrapSpan(text)}
                           </p>
@@ -137,29 +141,34 @@ const InstructionCards = () => {
               })}
 
               <div
-                className={`flex items-center ${
-                  currentIndex != 0 ? "justify-between" : "justify-end"
-                }`}
-              >
-                {currentIndex != 0 && (
+                className={`flex ${
+                  !currentIndex ? "justify-end" : "justify-between"
+                }  text-white items-center`}
+              > 
+                {currentIndex && (
                   <button
                     style={geometria.style}
-                    className={
-                      "text-[14px] text-[#141414] font-normal leading-[13px] lowercase px-[15px] py-[5px] border-[1px] border-[#D93284] rounded-[7px] bg-[#fff] hover:bg-[#D932841A]/[0.1] active:bg-[#F060C04D]/[0.3] transition-all ease-in duration-200"
-                    }
+                    className="pt-[3px] pb-[7px] px-[16px] text-[16px] leading-[14px] border-[1px] rounded-[8px] border-[#D93284] font-medium text-[#141414]
+                    md:text-[20px] md:leading-[0.9] md:px-[22px] md:border-[2px] md:rounded-[10px] lg:text-[22px] lg:leading-[1] lg:rounded-[12px]
+                    2xl:text-[30px] 2xl:leading-[0.9] 2xl:rounded-[16px] 2xl:pt-[8px] 2xl:pb-[13px] 2xl:px-[30px] transition-all duration-200 ease-in
+                    hover:bg-[#D932841A]/[0.1] active:bg-[#F060C04D]/[0.3] active:text-[#D93284]"
                     onClick={goToBackCard}
                   >
                     назад
                   </button>
                 )}
+
                 <button
                   style={geometriaMedium.style}
-                  className="text-[14px] text-white font-medium leading-[13px] lowercase px-[16px] py-[6px] border-none rounded-[7px] bg-[#d93284] hover:bg-[#F060C0] active:bg-[#792EC0] transition-all ease-in duration-200"
+                  className="pt-[4px] pb-[8px] px-[16px] text-[16px] leading-[14px] rounded-[8px] border-none bg-[#D93284] font-medium
+                  md:text-[20px] md:leading-[0.9] md:px-[22px] md:pt-[5px] md:pb-[9px] md:rounded-[10px] lg:text-[22px] lg:leading-[1] lg:rounded-[12px]
+                  2xl:text-[30px] 2xl:leading-[0.9] 2xl:rounded-[16px] 2xl:pt-[10px] 2xl:pb-[15px] 2xl:px-[30px] transition-all duration-200 ease-in
+                  hover:bg-[#F060C0] active:bg-[#792EC0]"
                   onClick={
-                    finishInstruction ? changeVisibleInstruction : goToNextCard
+                    !finishInstruction ? goToNextCard : changeVisibleInstruction
                   }
                 >
-                  {finishInstruction ? "начать" : "далее"}
+                  {!finishInstruction ? "далее" : "начать"}
                 </button>
               </div>
             </div>
