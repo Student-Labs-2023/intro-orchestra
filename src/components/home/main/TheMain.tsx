8fa09498c56492e9d52d606bbb1485a4fd55c234
Farkk <blazem.show@mail.ru>
2023-08-15 14:50:31 +0600

    fix

--- src/components/home/main/TheMain.tsx
+++ src/components/home/main/TheMain.tsx
@@ -6,7 +6,7 @@ const geometriaMedium = localFont({src: "../../../fonts/Geometria-Bold.woff",});
 
 export const TheMain = () => {
   return (
-    <main className="relative flex flex-col justify-center items-center text-center uppercase">
+    <main className="relative flex flex-col justify-center items-center text-center uppercase  min-h-screen">
       <h1
         style={geometriaBold.style}
         className="text-[25px] text-[#D93284] sm:text-[30px] md:text-[35px] lg:text-[45px] xl:text-[55px] 2xl:text-[70px]"
@@ -26,10 +26,14 @@ export const TheMain = () => {
         Нажмите на экран, чтобы начать
       </p>
       <div className="relative w-[30px] h-[60px] sm:w-[40px] sm:h-[80px] md:w-[50px] md:h-[100px] lg:w-[75px] lg:h-[140px]">
-        <div
-          className={` rounded-full ${styles.gradient} blur-[3px]`}
-        ></div>
-        <Image className="absolute w-[70%] h-[75%] top-[10%] left-[19%]" src={"/pointer.svg"} alt={"Указатель"} width={70} height={110}/>
+        <div className={` rounded-full ${styles.gradient} blur-[3px]`}></div>
+        <Image
+          className="absolute w-[70%] h-[75%] top-[10%] left-[19%]"
+          src={"/pointer.svg"}
+          alt={"Указатель"}
+          width={70}
+          height={110}
+        />
       </div>
     </main>
   );
