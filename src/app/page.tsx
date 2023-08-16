60b272c7795ef13ea3d5fd28d2eb882e0357001c
Farkk <blazem.show@mail.ru>
2023-08-17 03:09:58 +0600

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <Link href={"/panorama"} className="absolute block w-full h-full z-[1]" />
       <Image
         className={
-          "absolute h-screen object-contain translate-x-[-50%] translate-y-[-50%] top-2/4 left-2/4"
+          "absolute h-[100svh] object-contain translate-x-[-50%] translate-y-[-50%] top-2/4 left-2/4"
         }
         src={"/main-image.webp"}
         alt={"Картинка"}
