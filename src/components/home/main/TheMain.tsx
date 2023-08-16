1873a527fa133c9dbcdd26056aa09c21c4a115ed
Farkk <blazem.show@mail.ru>
2023-08-17 03:45:01 +0600

    fix-adaptive

--- src/components/home/main/TheMain.tsx
+++ src/components/home/main/TheMain.tsx
@@ -6,7 +6,7 @@ const geometriaMedium = localFont({src: "../../../fonts/Geometria-Bold.woff",});
 
 export const TheMain = () => {
   return (
-    <main className="relative flex flex-col justify-center items-center text-center uppercase h-[calc(100dvh-7.7dvh)]">
+    <main className="relative flex flex-col justify-center items-center text-center uppercase h-[calc(100dvh-9dvh)]">
       <h1
         style={geometriaBold.style}
         className="text-[7dvh] text-[#D93284]"
@@ -28,7 +28,7 @@ export const TheMain = () => {
       <div className="relative w-[3.91dvw] h-[12.7dvh]">
         <div className={` rounded-full ${styles.gradient} blur-[3px]`}></div>
         <Image
-          className="absolute w-[3.7dvw] h-[10.2dvh] top-[17%] left-[9.5%]"
+          className="absolute w-[3.7dvw] h-[10.2dvh] top-[17%] left-[9%]"
           src={"/pointer.svg"}
           alt={"Указатель"}
           width={70}
