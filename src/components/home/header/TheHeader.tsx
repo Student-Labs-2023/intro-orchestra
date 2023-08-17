25e71d0ff495cb5958f7e368a3a02359bd304db0
Farkk <blazem.show@mail.ru>
2023-08-17 14:04:52 +0600

    fix-adaptive

--- src/components/home/header/TheHeader.tsx
+++ src/components/home/header/TheHeader.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const TheHeader = () => {
   return (
-    <header className="flex justify-between items-center gap-[25px] flex-wrap mt-[2.8vh] mx-[5.5vh]">
+    <header className="flex justify-between items-center gap-[25px] flex-wrap mt-[4.1vh] mx-[4.1vh]">
       <Link href="https://omfil.ru" style={{ zIndex: "2" }}>
         <Image
           className="w-[13.5dvw]"
