539eb3c861cb9c23b9662dfc32130158d9b2a9e2
levickiy.143 <levickiy.143@gmail.com>
2023-08-24 22:43:31 +0600

    layouts upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          content="initial-scale=1, viewport-fit=cover"
         ></meta>
       </head>
       <body className="relative">{children}</body>
