32c5bf437d4aee13c3fb33a4cdfb737532284748
levickiy.143 <levickiy.143@gmail.com>
2023-08-24 22:27:17 +0600

    layouts upd

---
+++ src/app/panorama/layout.tsx
@@ -0,0 +1,23 @@
+import "./../globals.css";
+export const metadata = {
+  title: "Знакомство с Оркестром",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        ></meta>
+      </head>
+
+      <body className="relative">{children}</body>
+    </html>
+  );
+}
