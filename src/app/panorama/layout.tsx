121929fd7c275a83fab247dfce581a7902721096
Farkk <blazem.show@mail.ru>
2023-09-18 01:41:04 +0600

    fix

--- src/app/panorama/layout.tsx
+++ src/app/panorama/layout.tsx
@@ -1,24 +1,24 @@
-import "../globals.css";
-export const metadata = {
-  title: "Знакомство с Оркестром",
-};
+// import "../globals.css";
+// export const metadata = {
+//   title: "Знакомство с Оркестром",
+// };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ru">
-      <head>
-        <meta
-          name="viewport"
-          content="initial-scale=1, viewport-fit=cover"
-        ></meta>
-        <link rel="manifest" href="./manifest.json" />
-      </head>
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="ru">
+//       <head>
+//         <meta
+//           name="viewport"
+//           content="initial-scale=1, viewport-fit=cover"
+//         ></meta>
+//         <link rel="manifest" href="./manifest.json" />
+//       </head>
 
-      <body className="relative">{children}</body>
-    </html>
-  );
-}
+//       <body className="relative">{children}</body>
+//     </html>
+//   );
+// }
