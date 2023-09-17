f86cfb7b933033fce1d3589daca0614c53a4b7bb
levickiy.143 <levickiy.143@gmail.com>
2023-09-17 20:02:46 +0600

    try hideAddressBar

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -12,6 +12,26 @@ type TProps = {
 };
 
 const Artist = ({ params }: TProps) => {
+  window.onload = function () {
+    hideAddressBar();
+    window.addEventListener(
+      "orientationchange",
+      function () {
+        hideAddressBar();
+      },
+      false
+    );
+  };
+
+  function hideAddressBar() {
+    setTimeout(function () {
+      document.body.style.height = window.outerHeight + "px";
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 1100);
+    }, 1000);
+    return false;
+  }
   return (
     <>
       <ActivityCheck>
