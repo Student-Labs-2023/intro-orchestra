6eed8c8a66cf25e4db224411e8f1093da3085f3e
Farkk <blazem.show@mail.ru>
2023-09-22 22:06:52 +0600

    fix

--- src/app/panorama/layout.tsx
+++ src/app/panorama/layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+      <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover"
+        ></meta>
         <link rel="manifest" href="./manifest.json" />
       </head>
       <body className="relative">{children}</body>
